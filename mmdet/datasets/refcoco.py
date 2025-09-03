e1d49f0b96342b3a20e4d823d4643e6365f990f5
Suhel78601239 <suhelkhan78601239@gmail.com>
2025-09-03 17:27:02 +0530

    Update refcoco.py

--- mmdet/datasets/refcoco.py
+++ mmdet/datasets/refcoco.py
@@ -321,7 +321,7 @@ class RefCocoDataset(BaseDataset):
                 else:
                     raise ValueError(f'Invalid text mode "{self.text_mode}".')
                 ins = [{
-                    'mask': grounding_anno('segmentation', None),
+                    'mask': grounding_anno.get('segmentation', None),
                     'ignore_flag': 0
                 }] * len(text)
                 instances.extend(ins)
