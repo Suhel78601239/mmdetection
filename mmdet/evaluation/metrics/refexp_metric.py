c0ab739013512e47ef3ca4e5120e45cccab71b3e
Suhel78601239 <suhelkhan78601239@gmail.com>
2025-09-04 14:46:43 +0530

    Update refexp_metric.py

--- mmdet/evaluation/metrics/refexp_metric.py
+++ mmdet/evaluation/metrics/refexp_metric.py
@@ -67,18 +67,16 @@ class RefExpMetric(BaseMetric):
                 target_bbox[3] + target_bbox[1],
             ]
             #############
-            if idx == 0:
-                print("\n--- Debugging bbox formats ---")
-                print(f"Image ID: {img_id}")
-                print(f"Prediction sample: {result['bboxes'][0]}")
-                print(f"GT bbox (COCO [x,y,w,h]): {target_bbox}")
-                print(f"GT bbox (converted [x1,y1,x2,y2]): {converted_bbox}")
-                print("------------------------------\n")
+            print("\n--- Debugging bbox formats ---")
+            print(f"Image ID: {img_id}")
+            print(f"Prediction sample: {result['bboxes'][0]}")
+            print(f"GT bbox (converted [x1,y1,x2,y2]): {converted_bbox}")
+            print("------------------------------\n")
             ###############
             
             iou = bbox_overlaps(result['bboxes'],
                                 np.array(converted_bbox).reshape(-1, 4))
-            dataset_name = 'refcoco'
+            dataset_name = 'refcocog'
             for k in self.topk:
                 if max(iou[:k]) >= self.iou_thrs:
                     dataset2score[dataset_name][k] += 1.0
